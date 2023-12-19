7854d69e85a8268411b84570565916260d0e6831
reggyshicky <reginahshikanda@gmail.com>
2023-12-19 07:51:01 -0800

    mprove 12-log_stats.py by adding the top 10 of the most present IPs in the collection nginx of the database logs:

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+from pymongo import MongoClient
+"""
+Improve 12-log_stats.py by adding the top 10 of the most present IPs in 
+the collection nginx of the database logs:
+"""
+
+
+if __name__ == "__main__":
+    """provides stats about nginx logs"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    coll = client.logs.nginx
+    print("{} logs".format(coll.count_documents({})))
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = coll.count_documents({'method': method})
+        print("\tmethod {}: {}".format(method, count))
+
+    status_get = coll.count_documents({'method': 'GET', 'path': "/status"})
+    print("{} status check".format(status_get))
+
+    print("IPS:")
+    ips = coll.aggregate([
+        {"$group":
+            {
+                "_id": "$ip",
+                "count": {"$sum": 1}
+            }
+        },
+        {"$sort": {"count": -1}},
+        {"$limit": 10},
+        {"$project": {
+            "_id": 0,
+            "ip": "$_id",
+            "count": 1
+        }}
+    ])
+
+    for ip in ips:
+        print(f"\t{ip.get('ip')}: {ip.get('count')}")
